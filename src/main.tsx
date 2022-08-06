ecd5fd9c16b604bf7eff98a064a2fb78ed790799
Brian D <bdugg@keemail.me>
2022-08-06 18:07:09 -0400

    install react-query and setup initial data fetching

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.min.css";
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const client = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <QueryClientProvider client={client}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </QueryClientProvider>
   </React.StrictMode>
 );
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
-const Store = () => <h1>Store</h1>;
+import { useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+
+import {Product} from '../utils/types';
+
+const getProductCatalog = async (): Promise<Product[]> => 
+  await (await fetch('https://fakestoreapi.com/products')).json();
+
+const Store = () => {
+  const { data, isLoading, error } = useQuery<Product[]>(
+    ['products'],
+    getProductCatalog
+  );
+
+  return <h1>Store</h1>;
+};
 
 export default Store;
